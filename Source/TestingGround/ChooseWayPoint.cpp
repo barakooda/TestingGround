8b3bcd5f1438e554db4a4a894f849d232b72eae4
BAMBALAPTOP\Barakooda <barak00@gmail.com>
2017-09-13 22:11:28 +0300

    Creating PatrolRoute componen and using it, instead creating thridperson inheritance class.

--- Source/TestingGround/ChooseWayPoint.cpp
+++ Source/TestingGround/ChooseWayPoint.cpp
@@ -3,22 +3,33 @@
 #include "ChooseWayPoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "AIController.h"
-#include "PatrollingGuard.h"
+#include "PatrolRoute.h"
 EBTNodeResult::Type UChooseWayPoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) 
 {
 
 	
 	//Get blackboard component
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	
+	if (!ensure(PatrolRoute)) {	return EBTNodeResult::Failed ;}
+
+
+	
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0) 
+	{ 
+		UE_LOG(LogTemp, Warning, TEXT("No Patrol points")); 
+		return EBTNodeResult::Failed;
+	}
 
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
 
 	BlackboardComp->SetValueAsObject(WayPointKey.SelectedKeyName, PatrolPoints[Index]);
 	// todo protect agiasnt empty cases.
+	//protect against empty component
 	//UE_LOG(LogTemp, Warning, TEXT("WayPointIndex is : %i"),Index);
 
 	auto NextIndex = (Index + 1) % PatrolPoints.Num();
